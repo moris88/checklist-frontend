ad53c4fe062e177caffde88da56a16d6c9a26882
Maurizio Tolomeo <maurizio.tolomeo@outlook.it>
2024-10-05 15:19:51 +0200

    fix: error url

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,1 +1,4 @@
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL
+const VITE_SERVER_URL =
+  import.meta.env.VITE_SERVER_URL ?? 'http://localhost:5000'
+
+export const SERVER_URL = `${VITE_SERVER_URL}/api/v1`
